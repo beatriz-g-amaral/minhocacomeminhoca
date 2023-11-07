c3b74243d96b4957b765cbbea3f57a1c39de961a
Beatriz Amaral <104391703+beatriz-g-amaral@users.noreply.github.com>
2023-11-07 09:17:08 -0300

    fix main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
-#include "minhoca.h"
+#include <iostream>
+#include <thread>
+#include "minhoca.hpp"
+using namespace std;
 
 int main() {
     JogoMinhoca game(20, 20);
@@ -7,10 +10,10 @@ int main() {
         game.Setup();
         game.Input();
         game.Logic();
-        Sleep(100); 
+        this_thread::sleep_for(chrono::milliseconds(100));
     }
 
-    std::cout << "Dormiu no ponto! Seus pontos:" << game.IsGameOver() << std::endl;
+    cout << "Dormiu no ponto! Seus pontos:" << game.IsGameOver() << endl;
 
     return 0;
 }
